c8d64dbe44bc87c13293bc46aed244d8548b5656
Agatha França <agathafranca@outlook.com>
2022-05-13 15:17:48 -0300

    Exemplifies operator precedence and concatenation

---
+++ 3-arithmeticOperations.js
@@ -0,0 +1,8 @@
+console.log("Arithmetic operations");
+
+console.log(2 + 2);
+console.log(10 + 8 * 2); //standard precedence of mathematics
+console.log((10 + 8) * 2); //"forced" precedence by parentheses 
+
+console.log("year " + 2020)
+console.log("2" + "2")
