89b8dea5f088899ff35d8d8f008e2cc61aadf1d2
Agatha França <agathafranca@outlook.com>
2022-05-13 14:32:07 -0300

    Save values, does math operations and displays

--- 1-myFirstProgram.js
+++ 1-myFirstProgram.js
@@ -1,1 +1,13 @@
-console.log("My first program");
+console.log("My first program: working with variables");
+
+const age = 26;
+
+console.log("age");
+console.log(age);
+console.log(age + 2);
+console.log(age - 2);
+console.log(age / 2);
+console.log(age * 2);
+
+const ageAddedUp = age + 2;
+console.log(ageAddedUp)
