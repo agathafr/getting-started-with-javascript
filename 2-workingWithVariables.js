75aeab49c1300b1f9487b01c0170e9bea7e2c48a
Agatha França <agathafranca@outlook.com>
2022-05-13 15:10:02 -0300

    Exemplifies not desirable statement

--- 2-workingWithVariables.js
+++ 2-workingWithVariables.js
@@ -4,4 +4,6 @@ const age = 29;
 const name = "Ricardo";
 let year = 2000;
 
-console.log(age);
+// don't do because not putting "const" raises the variable to a general scope, which makes traceabillity difficult
+// month = "January"
+
