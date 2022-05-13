b4e78c00e0e06f96cdffcfb30b386b4a45a47364
Agatha França <agathafranca@outlook.com>
2022-05-13 16:51:37 -0300

    Declares a list and adds items to it

---
+++ 6-lists.js
@@ -0,0 +1,20 @@
+console.log(`Working with lists`);
+// const salvador = `Salvador`;
+// const saoPaulo = `Sao Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+//ctrl + k + c / ctrl + k + u
+
+const listOfDestinations = new Array(
+  `Salvador`,
+  `Sao Paulo`,
+  `Rio de Janeiro`
+);
+
+listOfDestinations.push( //adds items to the list
+  `Curitiba`
+);
+
+console.log("Possible destinations");
+//console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listOfDestinations);
