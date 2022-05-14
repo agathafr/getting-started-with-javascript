32d7d9410d50cd9c5c63a9a9f36803268d7cc60d
Agatha França <agathafranca@outlook.com>
2022-05-14 07:06:44 -0300

    Examples of if chaining

--- 7-conditional.js
+++ 7-conditional.js
@@ -1,21 +1,29 @@
 console.log("Working with conditionals");
 
 const listOfDestinations = new Array(`Salvador`, `Sao Paulo`, `Rio de Janeiro`);
-const ageOfBuyer = 15;
+
+const ageOfBuyer = 18;
+const isAccompanied = false;
 console.log("Possible destinations:");
 console.log(listOfDestinations);
 
 if (ageOfBuyer >= 18) {
   console.log("Buyer of legal age!");
-  listOfDestinations.splice(1, 1);
+  listOfDestinations.splice(1, 1); //remove item
 } else {
-  console.log("Buyer is not of legal age! I can't sell.");
+  //The person is a minor
+  if (isAccompanied) {
+    console.log("Buyer is accompanied!");
+    listOfDestinations.splice(1, 1); //remove item
+  } else {
+    console.log("Buyer is not of legal age! I can't sell.");
+  }
 }
 
 console.log(listOfDestinations);
 
-console.log(ageOfBuyer > 18);
-console.log(ageOfBuyer < 18);
-console.log(ageOfBuyer <= 18);
-console.log(ageOfBuyer >= 18);
-console.log(ageOfBuyer == 18);
+// console.log(ageOfBuyer > 18);
+// console.log(ageOfBuyer < 18);
+// console.log(ageOfBuyer <= 18);
+// console.log(ageOfBuyer >= 18);
+// console.log(ageOfBuyer == 18);
