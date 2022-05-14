e721909026a24c263c914b308c510ded125452d2
Agatha França <agathafranca@outlook.com>
2022-05-14 09:43:46 -0300

    Exemplifies the use of debbuger and break

--- 8-loops.js
+++ 8-loops.js
@@ -5,23 +5,22 @@ const listOfDestinations = new Array(`Salvador`, `Sao Paulo`, `Rio de Janeiro`);
 const ageOfBuyer = 18;
 const isAccompanied = false;
 let hasPurchasedTicket = false;
-const destination = "Salvador"
+const destination = "Salvador";
 
 console.log(" \n Possible destinations:");
 console.log(listOfDestinations);
 
-if (ageOfBuyer >= 18 || isAccompanied) {
-  console.log("Buyer of legal age or is accompanied. Good trip!");
-  listOfDestinations.splice(1, 1); //remove item
-  hasPurchasedTicket = true;
-} else {
-    console.log("Buyer is not of legal age! I can't sell.");
-    hasPurchasedTicket = false;
-  }
+const canBuy = ageOfBuyer >= 18 || isAccompanied;
 
-console.log("Board: \n\n")
-if(ageOfBuyer >= 18 || isAccompanied && hasPurchasedTicket) {
-  console.log("You can board. Good trip!");
-} else {
-  console.log("You cann't board!");
+let count = 0;
+let destinyExists = false;
+
+while (count < 3) {
+  if (listOfDestinations[count] == destination) {
+    destinyExists = true;
+    break;
+  }
+  count += 1;
 }
+
+console.log("Existing destination: ", destinyExists);
